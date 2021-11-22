8e7e0c5a14f79f50d513d889f4449e84b65f57c0
Josh Hester <johest@mlc176.rmt.sas.com>
2021-11-22 15:02:14 -0500

    initial completion

--- src/main/firefighters/FireDispatchImpl.java
+++ src/main/firefighters/FireDispatchImpl.java
@@ -1,35 +1,64 @@
 package main.firefighters;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import main.api.Building;
 import main.api.City;
 import main.api.CityNode;
 import main.api.FireDispatch;
 import main.api.Firefighter;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import main.api.exceptions.NoFireFoundException;
 
 public class FireDispatchImpl implements FireDispatch {
 
+  private List<Firefighter> fireFighters;
+  private City city;
+
   public FireDispatchImpl(City city) {
-    // TODO
-    throw new NotImplementedException();
+    this.city = city;
   }
 
   @Override
   public void setFirefighters(int numFirefighters) {
-    // TODO
-    throw new NotImplementedException();
+    ArrayList<Firefighter> fireFighters = new ArrayList<Firefighter>(numFirefighters);
+    for (int i = 0; i < numFirefighters; i++) {
+    	FirefighterImpl f = new FirefighterImpl(city.getFireStation());
+    	fireFighters.add(f);
+    }
+    this.fireFighters = fireFighters;
   }
 
   @Override
   public List<Firefighter> getFirefighters() {
-    // TODO
-    throw new NotImplementedException();
+    return this.fireFighters;
   }
 
   @Override
   public void dispatchFirefighers(CityNode... burningBuildings) {
-    // TODO
-    throw new NotImplementedException();
+    for (CityNode building : burningBuildings) {
+    	FirefighterImpl fighter = getClosestFireFighter(building);
+    	fighter.setLocation(building);
+    	Building b = city.getBuilding(building);
+    	try {
+			b.extinguishFire();
+		} catch (NoFireFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+  }
+
+  private FirefighterImpl getClosestFireFighter(CityNode building) {
+	FirefighterImpl closestFighter = (FirefighterImpl) getFirefighters().get(0);
+	int closestDistance = closestFighter.calculateDistance(building);
+	for (Firefighter f : getFirefighters()) {
+		int currentDistance = ((FirefighterImpl)f).calculateDistance(building);
+		if (currentDistance<closestDistance) {
+			closestDistance = currentDistance;
+			closestFighter = (FirefighterImpl) f;
+		}
+	}
+	return closestFighter;
   }
 }
--- src/main/firefighters/FirefighterImpl.java
+++ src/main/firefighters/FirefighterImpl.java
@@ -1,20 +1,46 @@
 package main.firefighters;
 
+import main.api.Building;
 import main.api.CityNode;
 import main.api.Firefighter;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class FirefighterImpl implements Firefighter {
-
+  private int distance;
+  private CityNode location;
+  
+  FirefighterImpl(Building fireStation) {
+	  setFireStation(fireStation);
+	  setDistance(0);
+  }
+  
   @Override
   public CityNode getLocation() {
-    // TODO
-    throw new NotImplementedException();
+    return location;
   }
 
   @Override
   public int distanceTraveled() {
-    // TODO
-    throw new NotImplementedException();
+    return this.distance;
+  }
+
+  private void setFireStation(Building fireStation) {
+	  this.location = fireStation.getLocation();
+  }
+
+  private int getDistance() {
+    return this.distance;
+  }
+  
+  private void setDistance(int distance) {
+	  this.distance = distance;
+  }
+  
+  public void setLocation(CityNode building) {
+	  setDistance(getDistance() + calculateDistance(building));
+	  this.location = building;
+  }
+
+  public int calculateDistance(CityNode building) {
+	return Math.abs(getLocation().getX()-building.getX()) + Math.abs(getLocation().getY()-building.getY());
   }
 }
